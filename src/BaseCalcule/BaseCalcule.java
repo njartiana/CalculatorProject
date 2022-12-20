89fc006f30a96ad395e081dd55bda83b525867ea
njartiana <48241768+njartiana@users.noreply.github.com>
2022-12-20 10:19:16 +0300

    Commit update BaseCalcule, Main, TestCalcule.java

--- src/BaseCalcule/BaseCalcule.java
+++ src/BaseCalcule/BaseCalcule.java
@@ -1,6 +1,9 @@
 package BaseCalcule;
 
 public class BaseCalcule {
+    private float value_1;
+    private float value_2;
+
     public int add(int value_1, int value_2){
         return value_1 + value_2 ;
     }
@@ -10,5 +13,9 @@ public class BaseCalcule {
 
     public int multiplication(int value_1, int value_2){
         return value_1 * value_2;
+    }
+
+    public float divide(float value_1, float value_2){
+        return value_1 / value_2;
     }
 }
