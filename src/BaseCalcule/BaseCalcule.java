efeb645d9c2b203a39ad7e7fd7fa7453211d9adf
njartiana <48241768+njartiana@users.noreply.github.com>
2022-12-20 10:22:14 +0300

    Commit update BaseCalcule

--- src/BaseCalcule/BaseCalcule.java
+++ src/BaseCalcule/BaseCalcule.java
@@ -9,10 +9,10 @@ public class BaseCalcule {
     }
 
     public int multiplication(int value_1, int value_2){
-        return value_1 * value_2;
+        return value_1 * value_2 ;
     }
 
     public float divide(float value_1, float value_2){
-        return value_1 / value_2;
+        return value_1 / value_2 ;
     }
 }
