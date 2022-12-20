52432c765a005db8092c388a3f4106c34747d454
njartiana <48241768+njartiana@users.noreply.github.com>
2022-12-20 09:37:30 +0300

    Commit update code

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Test.TestCalcule;
+
+public class Main {
+    public static void main(String[] args) {
+        TestCalcule test = new TestCalcule();
+        test.test_add();
+        test.test_multiplication();
+        test.test_substract();
+    }
+}
